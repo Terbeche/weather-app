2d9e53c925e01a4cfb5d203a9d22abfc57ef13f5
Mostefa-Terbeche <termus96@gmail.com>
2024-07-14 19:48:30 +0100

    Update setup for heroku

--- src/api/main.py
+++ src/api/main.py
@@ -26,3 +26,8 @@ app.include_router(dashboard_locations.router)
 
 Base.metadata.create_all(bind=engine)
 add_locations_to_db()
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
